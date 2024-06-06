e0dd3f5a611762f684dfdad1823619477df82b77
mariobgsp <mariobgsp@gmail.com>
2024-06-06 14:00:16 +0700

    add databases initiator

--- gateway-service/src/main/java/com/example/gatewayservice/repository/UserRepository.java
+++ gateway-service/src/main/java/com/example/gatewayservice/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.gatewayservice.repository;
 
 import com.example.gatewayservice.models.entity.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
