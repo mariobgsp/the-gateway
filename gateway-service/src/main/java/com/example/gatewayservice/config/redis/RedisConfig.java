796f48474803846c00edc930c1228c52e23615ab
Muhammad Ario Bagus Prakusa <mariobgsp@gmail.com>
2024-08-16 15:06:24 +0700

    redis for props

--- gateway-service/src/main/java/com/example/gatewayservice/config/redis/RedisConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/redis/RedisConfig.java
@@ -1,39 +1,4 @@
 package com.example.gatewayservice.config.redis;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-
-@Configuration
-public class RedisConfig {
-    @Value("${redis.host}")
-    private String redisHost;
-
-    @Value("${redis.port}")
-    private int redisPort;
-
-    @Value("${redis.password}")
-    private String redisPassword;
-
-    @Value("${redis.database}")
-    private int redisDatabase;
-
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
-        config.setPassword(redisPassword);
-        config.setDatabase(redisDatabase);
-        return new JedisConnectionFactory(config);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
-
+public class RedisCache {
 }
--- gateway-service/src/main/java/com/example/gatewayservice/repository/RedisKeyValueRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.gatewayservice.repository;
-
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
-
-public interface RedisKeyValueRepository extends KeyValueRepository<String, Object> {
-}
